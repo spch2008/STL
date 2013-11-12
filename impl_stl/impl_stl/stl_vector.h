f527e3dce8d4ad25f531b9ee21218abb3797ce75
cheng <spch2008@foxmail.com>
2013-11-12 16:02:52 +0800

    add stl_vector

---
+++ impl_stl/impl_stl/stl_vector.h
@@ -0,0 +1,14 @@
+
+template <class T>
+class vector
+{
+public:
+	typedef T  value_type;
+	typedef T* pointer;
+	typedef T& reference;
+	typedef size_t size_type;
+	typedef ptrdiff_t difference_type;
+
+public:
+	vector()
+};
